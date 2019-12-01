292c9a96ceb45700ee991520f4a461d32209cdbb
Grzegorz Konarski <gk@pubfront.com>
2019-12-01 23:01:56 +0100

    bump

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import WelcomePage from './WelcomePage';
 
 export default class App extends Component {
