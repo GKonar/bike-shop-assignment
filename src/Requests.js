53d1c59a1a51d4dba1be23507cfc9e7b8973c7eb
Grzegorz Konarski <gk@pubfront.com>
2019-12-10 14:36:40 +0100

    Create Request.js module to manage api calls

---
+++ src/Requests.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const apiUrl = 'http://localhost:3000';
+
+class Requests {
+  constructor(apiUrl) {
+    this.apiUrl = apiUrl;
+  }
+
+  async getBike(bikeId) {
+    const bikeUrl = `${this.apiUrl}/bikes/${bikeId}`;
+    const response = await axios.get(bikeUrl);
+    return response.data;
+  }
+
+  async getBikes() {
+    const bikesUrl = `${this.apiUrl}/bikes`;
+    const response = await axios.get(bikesUrl);
+    return response.data;
+  }
+}
+
+const requests = new Requests(apiUrl);
+export default requests;
