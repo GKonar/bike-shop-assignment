d32e111a3dcbc59bf3cd3eb49b9dca6d4fcb6500
Grzegorz Konarski <gk@pubfront.com>
2019-12-24 14:36:20 +0100

    bump

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -12,19 +12,20 @@ class WelcomePage extends Component {
   render() {
     const { classes, basketItems, removeFromBasket, basketTotal, basketQuantity } = this.props;
     return (
-      <div className={classes.root}>
+      <div className={ classes.root }>
         <Navbar 
-          basketItems={basketItems}
-          removeFromBasket={removeFromBasket}
-          basketTotal={basketTotal}/>
-        <div className={classes.welcomeContainer}>
-          <span className={classes.welcomeText}>
-            <img src={`${welcomePageTextLogo}`} alt="logo"/>
-            <h3 className={classes.header}>A NEW GENERATION OF VINTAGE BIKE</h3>
+          basketItems={ basketItems }
+          removeFromBasket={ removeFromBasket }
+          basketTotal={ basketTotal }
+          basketQuantity={ basketQuantity }/>
+        <div className={ classes.welcomeContainer }>
+          <span className={ classes.welcomeText }>
+            <img src={ `${welcomePageTextLogo}` } alt="logo"/>
+            <h3 className={ classes.header }>A NEW GENERATION OF VINTAGE BIKE</h3>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been    the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
          </span>
-         <Link className={classes.link} to="/shop">
-            <Button className={classes.button} variant="outlined">
+         <Link className={ classes.link } to="/shop">
+            <Button className={ classes.button } variant="outlined">
               EXPLORE THE SHOP &nbsp;&nbsp;&nbsp;  <Arrow/>
             </Button>
          </Link>
