37380f348c9f94e3c855a72a0454e5cfa143a18c
Grzegorz Konarski <gk@pubfront.com>
2019-12-28 14:16:41 +0100

    Render Faq questions

--- src/Faq.js
+++ src/Faq.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 import { withStyles } from '@material-ui/styles';
+import { Link } from 'react-router-dom';
+import uuid from 'uuid';
 
-import faqImage from './assets/images/customer_1.jpeg'
+import { minTwoDigits } from './helpers';
+
+import faqImage from './assets/images/black_white.jpeg'
 
 const styles = {
   faq: {
@@ -10,25 +14,75 @@ const styles = {
     backgroundSize: "cover",
     width: "100%",
     height: "100vh",
-    filter: "brightness(200%)"
+    filter: "brightness(100%)",
+    position:"relative"
+  },
+  faqHeader: {
+    color: "#64c29e",
+    width: "100%",
+    marginLeft: "2rem",
   },
   faqList: {
     width: "60%",
-    padding: "3rem",
+    height: "100%",
     margin: "0 auto",
-    backgroundColor: "rgba(0, 0, 0, 0.6)",
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    overflow: "scroll",
+  },
+  faqListItem: {
+    color: "#fff",
+    width: "40%",
+    padding: "0 1rem",
+    fontSize: ".9rem"
+  },
+  faqListItemHeader: {
+    color: "#64c29e"
+  },
+  faqQuestion: {
+    
+  },
+  faqGoBack: {
+    position: "fixed",
+    left: "5%",
+    top: "5%",
+    color: "#fff",
+    textDecoration: "none",
+    fontSize: "1.5rem",
+    fontWeight: 400,
+    padding: "1rem 2rem",
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
+    transition: ".2s",
+    letterSpacing: "1px",
+    "&:hover" : {
+      color: "#64c29e"
+    }
   }
 }
 
 class Faq extends Component {
   render() {
-    const { classes } = this.props;
+    const { classes, faq } = this.props;
 
     return (
-      <div className={classes.faq}>
-        <div className={classes.faqList}>
-        I’ve found the sneakers I want but can’t select my size
-        I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.
+      <div className={ classes.faq }>
+        <Link to="/customer" className={ classes.faqGoBack }>BACK</Link>
+        <div className={ classes.faqList }>
+        <h1 className={ classes.faqHeader }>FAQ</h1>
+          {
+            faq.map((q, i) => {
+              return (
+                <div 
+                  className={classes.faqListItem}
+                  key={uuid()}>
+                  <h2 className={classes.faqListItemHeader}>{minTwoDigits(i + 1)}</h2>
+                  <h3 className={classes.faqQuestion}>{q.question}</h3>
+                  <p className={classes.faqAnswer}>{q.answer}</p>
+                </div> 
+              )})
+          }
         </div>
       </div>
     )
